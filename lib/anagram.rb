f4ba4ba8a07337db17489b962427f94aaf585cb2
Jonathan Hong <jonhong777@gmail.com>
2018-07-11 21:26:29 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,12 +2,12 @@
 class Anagram
   
   def initialize(word)
-    
+    @word = word
   end
   
   def match(anagrams)
     anagrams.select do |scrambled|
-      
+      @word.split
     end
   end
 end
